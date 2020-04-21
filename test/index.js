b454ae2034cc1b90e8991db565bcabccb604f51a
stephaniebang <ej.bang27@gmail.com>
2020-04-21 15:55:29 -0300

    Test other input files

--- test/index.js
+++ test/index.js
@@ -1,48 +1,52 @@
+const fs = require('fs')
 const assert = require('assert')
-const fns = require('../index')
+const { csvToJson } = require('../index')
 
-describe('getAddresses()', () => {
-  it('should return an array with 1 object', () => {
-    const colName = 'email Responsável, Pai'
-    const col = 'johndoepai1@gmail.com :)'
-    const correct = [{
-      "type": "email",
-      "tags": [
-        "Responsável",
-        "Pai"
-      ],
-      "address": "johndoepai1@gmail.com"
-    }]
+const sort = obj => {
+  const classes = obj.classes.sort()
+  const addresses = obj.addresses.sort((a, b) => {
+    if (a.address < b.address) return 1
+    if (a.address > b.address) return -1
+    return 0
+  }).map(a => a.tags = a.tags.sort())
 
-    // fns.getAddress('johndoepai2@gmail.com/johndoepai3@gmail.com', 'email Mãe')
+  return { ...obj, classes, addresses }
+}
 
-    assert.deepEqual(correct, fns.getAddresses(col, colName))
+describe('csvToJson()', () => {
+  it('should match the challenge\'s output example', () => {
+    csvToJson('input.csv')
+
+    const correct = JSON.parse(fs.readFileSync('./test/output_example.json', 'utf8'))
+    const result = JSON.parse(fs.readFileSync('./output.json', 'utf8'))
+
+    assert.deepEqual(correct, result)
   })
-})
 
-describe('getAddresses()', () => {
-  it('should return an array with 2 objects', () => {
-    const colName = 'email Responsável, Pai'
-    const col = 'johndoepai2@gmail.com/johndoepai3@gmail.com'
-    const correct = [
-      {
-        "type": "email",
-        "tags": [
-          "Responsável",
-          "Pai"
-        ],
-        "address": "johndoepai2@gmail.com"
-      },
-      {
-        "type": "email",
-        "tags": [
-          "Responsável",
-          "Pai"
-        ],
-        "address": "johndoepai3@gmail.com"
-      }
-    ]
+  it('should join repeated entries with repeated tags correctly', () => {
+    csvToJson('./test/input_repeated.csv')
 
-    assert.deepEqual(correct, fns.getAddresses(col, colName))
+    const correct = JSON.parse(fs.readFileSync('./test/output_repeated.json', 'utf8'))
+    const result = JSON.parse(fs.readFileSync('./output.json', 'utf8'))
+
+    assert.deepEqual(correct, result)
+  })
+
+  it('should return the same content despite column order', () => {
+    csvToJson('./test/input_order.csv')
+
+    const correct = JSON.parse(fs.readFileSync('./test/output_example.json', 'utf8'))
+    const result = JSON.parse(fs.readFileSync('./output.json', 'utf8'))
+
+    assert.deepEqual(sort(correct[0]), sort(result[0]))
+  })
+
+  it('should return addresses with correct tags', () => {
+    csvToJson('./test/input_tags.csv')
+
+    const correct = JSON.parse(fs.readFileSync('./test/output_tags.json', 'utf8'))
+    const result = JSON.parse(fs.readFileSync('./output.json', 'utf8'))
+
+    assert.deepEqual(correct, result)
   })
 })
